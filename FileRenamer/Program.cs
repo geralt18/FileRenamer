e096283d598e983aec3cd3f1421a49aaf8d33a04
geralt18 <geralt18@gmail.com>
2020-10-27 09:36:23 +0100

    Add project files.

---
+++ FileRenamer/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.IO;
+
+namespace FileRenamer
+{
+   class Program
+   {
+      static void Main(string[] args) {
+
+         foreach (var file in Directory.GetFiles(_path)) {
+            string name = Path.GetFileNameWithoutExtension(file);
+            string ext = Path.GetExtension(file);
+
+            string[] vs = name.Split("_");
+            int chapter = int.Parse(vs[0]);
+            int page = int.Parse(vs[1]);
+            string newName = $"{chapter:d3}_{page:d2}{ext}";
+            Console.WriteLine($"Zmieniam nazwę pliku {file} -> {newName}");
+            FileSystem.Rename(Path.Combine(_path, file), Path.Combine(_path, newName));
+         }
+      }
+
+      static string _path = @"D:\Temp\Download\Naruto Manga";
+   }
+}
